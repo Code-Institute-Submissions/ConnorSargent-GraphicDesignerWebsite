3dc19c1e4a9f8b8d5699c018b085eeec37d8296f
ConnorSargent <consargent@gmail.com>
2022-01-27 18:28:31 +0000

    added basic checkout functionality

--- checkout/models.py
+++ checkout/models.py
@@ -8,8 +8,9 @@ from products.models import Product
 
 # Create your models here.
 
+
 class Order(models.Model):
-    
+
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -25,13 +26,11 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
-
     def _generate_order_number(self):
         """
         Generate a random 32 char string (Unique order number)
         """
         return uuid.uuid4().hex.upper()
-
 
     def save(self, *args, **kwargs):
         """
@@ -41,10 +40,8 @@ class Order(models.Model):
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
-
     def __str__(self):
         return self.order_number
-
 
     def update_total(self):
         """
@@ -58,14 +55,11 @@ class Order(models.Model):
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
-
-
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-
 
     def save(self, *args, **kwargs):
         """
@@ -73,7 +67,6 @@ class OrderLineItem(models.Model):
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
-
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
