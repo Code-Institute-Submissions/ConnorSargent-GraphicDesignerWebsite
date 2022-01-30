79838d251e6e560afabc37b51ad8bd13659e98fa
ConnorSargent <consargent@gmail.com>
2022-01-30 00:26:40 +0000

    removed secret key and set debug

--- graphic_designer_website/settings.py
+++ graphic_designer_website/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d_=&s!#060za#7k921l+(vdgwvsph$8rz$!0ryo8p1d^d6z0ty'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['graphic-designer-website.herokuapp.com', 'localhost']
 
