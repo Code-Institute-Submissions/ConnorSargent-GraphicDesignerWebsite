4ac15a0fdc52589867f8cd4945e4fc8de4f42397
ConnorSargent <consargent@gmail.com>
2022-01-30 00:11:04 +0000

    connected to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn graphic-designer-website.wsgi:application
--- graphic_designer_website/settings.py
+++ graphic_designer_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d_=&s!#060za#7k921l+(vdgwvsph$8rz$!0ryo8p1d^d6z0ty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://git.heroku.com/graphic-designer-website.git]
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'graphic_designer_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
