ba8f8b6579dcf1e12fcdf2e9e5c36cbe154dc878
ConnorSargent <consargent@gmail.com>
2022-02-21 18:23:50 +0000

    Add Upcoming post functionality

--- upcoming/urls.py
+++ upcoming/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upcoming, name='home')
+    path('', views.upcoming, name='upcoming'),
+    path('addpost/', views.add_post, name="add_post"),
 ]
--- upcoming/views.py
+++ upcoming/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from datetime import datetime
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 
-def upcoming(request):
-    """ A view to return the upcoming page """
 
-    return render(request, 'upcoming/upcoming.html')
+def upcoming(request):
+    posts = Post.objects.all()
+
+    context = {
+        "posts": posts,
+    }
+
+    """ A view to return the upcoming page """
+    return render(request, 'upcoming/upcoming.html', context)
+
+
+def add_post(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = PostForm(request.POST or None)
+            if form.is_valid():
+                data = form.save(commit=False)
+                data.title = request.POST["title"]
+                data.content = request.POST["content"]
+                data.date_created = datetime.now()
+                data.user = request.user
+                data.save()
+                messages.info(request, "Upcoming Post Added")
+                return redirect("upcoming")
+        else:
+            form = PostForm()
+        return render(request, "upcoming", {"form": form})
